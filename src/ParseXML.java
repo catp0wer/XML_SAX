76b51ba6b2367c84c0e819c3b778a86c23e6d31b
laila <laila@fake.com>
2018-03-04 11:02:44 +0200

    added main

--- src/ParseXML.java
+++ src/ParseXML.java
@@ -1,5 +1,30 @@
 
+import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
 public class ParseXML {
 
+    public static void main(String[] args){
+        try {
+            File my_file = new File("music.xml");
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            Handler my_Handler = new Handler();
+            saxParser.parse(my_file,my_Handler);
+
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
